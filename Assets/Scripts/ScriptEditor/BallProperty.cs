dfd30122146fc4abb523f362788518133001eef0
jacoposcuola <jacopo.scuola0@gmail.com>
2021-03-26 23:51:46 +0100

    change color ball (tools)

---
+++ Assets/Scripts/ScriptEditor/BallProperty.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class BallProperty : MonoBehaviour
+{
+   public GameObject ball;
+
+    
+    public void ChangeColor()
+    {
+        GetComponentInChildren<Renderer>().material.color = Random.ColorHSV();
+    }
+
+    public void ResetColorToOriginal()
+    {
+        GetComponentInChildren<Renderer>().material.color = Color.white;
+    }
+    
+}
---
+++ Assets/Scripts/ScriptEditor/EditorTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(BallProperty))]
+public class EditorTest : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        BallProperty ballProp = (BallProperty)target;
+
+        DrawDefaultInspector();
+
+        if(GUILayout.Button("Genera Colore"))
+        {
+            ballProp.ChangeColor();
+        }
+
+        if(GUILayout.Button("Reset Colore"))
+        {
+            ballProp.ResetColorToOriginal();
+        }
+        
+    }
+
+
+
+
+}
