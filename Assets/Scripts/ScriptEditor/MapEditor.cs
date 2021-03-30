72e61e973f65f769a53cecc7537acae1e797ebb5
jacoposcuola <jacopo.scuola0@gmail.com>
2021-03-31 01:09:41 +0200

    test Tools PR

---
+++ Assets/Scripts/ScriptEditor/MapEditor.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using jsFramework;
+
+[CustomEditor(typeof(GridGame))]
+public class MapEditor : Editor
+{
+    private SerializedObject objectSerialize;
+    private SerializedProperty prop;
+
+    //public GameObject cube;
+
+    private void OnSceneGUI()
+    {
+        objectSerialize = new SerializedObject(target);
+
+    }
+
+
+    public override void OnInspectorGUI()
+    {
+        GridGame gridGame = (GridGame)target;
+
+        DrawDefaultInspector();
+
+        if(GUILayout.Button("Genera Mappa"))
+        {
+            gridGame.GenerateMap();
+        }
+
+        objectSerialize.Update();
+
+        prop = objectSerialize.FindProperty("GenerateMap");
+        EditorGUILayout.PropertyField(prop); // la assegna
+
+        objectSerialize.ApplyModifiedProperties(); // app
+
+    }
+
+    public void OnDestroy()
+    {
+       
+    }
+
+
+}
---
+++ Assets/Scripts/ScriptEditor/TestMenuItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class TestMenuItem : Editor
+{
+    [MenuItem("Cesto/Rigidbody/MyMenu")]
+   private static void NewOption(MenuCommand menuCommand)
+   {
+        Rigidbody rb = menuCommand.context as Rigidbody;
+   }
+
+
+}
